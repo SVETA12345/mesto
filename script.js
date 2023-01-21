058ed383f211f6839e216acd1162e8de681a2750
Svetlana Martyanova <smartianova8@.mail.ru>
2023-01-21 23:27:07 +0400

    proba 1

---
+++ script.js
@@ -0,0 +1,36 @@
+const popupBtnOpen=document.querySelector('.button__edit');
+const popup=document.querySelector('.popup')
+const popupsave=document.querySelector('.button__save')
+const popupclose=document.querySelector('.popup__close')
+
+
+let formElement=document.querySelector('.form')
+let nameInput=document.querySelector('.popup__name')
+let jobInput=document.querySelector('.popup_second-name');
+popupBtnOpen.addEventListener('click',openPopup);
+popupsave.addEventListener('click', handleFormSubmit);
+popupsave.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
+popupclose.addEventListener('click',closePopup);
+function handleFormSubmit (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+ 
+     // Получите значение полей jobInput и nameInput из свойства value                                               // О том, как это делать, расскажем позже.
+    // Выберите элементы, куда должны быть вставлены значения полей
+    let name=document.querySelector('.profile__name');
+    let job=document.querySelector('.profile__second-name');
+  // Вставьте новые значения с помощью textContent
+    name.textContent=nameInput.value
+    job.textContent=jobInput.value
+  
+}
+
+function openPopup(){
+
+    popup.classList.add('popup_opened');
+}
+function closePopup(){
+    popup.classList.remove('popup_opened');
+}
+ 
